a6255c406cd3d4fdf28774037e163edec4c5e002
glabojko <glabojko@gmail.com>
2023-02-18 00:32:18 +0100

    tictactoe.py

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, random
 
 def cls():
   os.system('cls||clear')
@@ -16,7 +16,7 @@ def get_empty_board():
   return board
 
 def get_human_coordinates(character, board):
-  human_input_row = input("Wprowadz literę rzędu (od A do C"
+  human_input_row = input("Wprowadź literę rzędu (od A do C"
                           ")")
   if human_input_row.lower() == "quit":
     quit()
@@ -26,7 +26,7 @@ def get_human_coordinates(character, board):
                             ")")
 
   try:
-    human_input_col = int(input("Wprowadz kolumnę (od 1 do 3"
+    human_input_col = int(input("Wprowadź kolumnę (od 1 do 3"
                                 ")"))
     while human_input_col != 1 and human_input_col != 2 and human_input_col != 3:
       human_input_col = int(
@@ -52,7 +52,6 @@ def get_human_coordinates(character, board):
     "C2": (2, 1),
     "C3": (2, 2)
   }
-
   human_input_row = human_input_row.upper()
   row_coordinate = human_input_row
   col_coordinate = human_input_col
@@ -85,6 +84,7 @@ def display_board(board):
 def is_board_full(board):
   return not any("." in sl for sl in board)
 
+
 def check_win(board):
   if board[0][0] == 'X' and board[0][1] == 'X' and board[0][2] == 'X' or board[
       0][0] == 'O' and board[0][1] == 'O' and board[0][2] == 'O':
@@ -112,13 +112,14 @@ def check_win(board):
     return True
   else:
     return False
-
+    
 def menu_again():
   again = input("Chcesz zagrać jeszcze raz? (t/n): ")
   if again.lower() == "t":
-    game()
+    main_menu()
   else:
     quit()
+
 
 def main_menu():
   print(
@@ -128,10 +129,7 @@ def main_menu():
     "Wybierz czy chcesz zagrać z komputerem czy człowiekiem \n('k' - komputer/ 'c' - człowiek): "
   )
   if oponent.lower() == "k":
-    print(
-      "Niestety tej opcji nie udało nam się jeszcze zakodować. Pracujemy nad tym."
-    )
-    main_menu()  # AI_game()
+    whos_first()
   elif oponent.lower() == "c":
     game()
   else:
@@ -144,11 +142,22 @@ def choose_character():
     "Wybierz czy chcesz zacząć grać kółkiem ('O') czy krzyżykiem ('X'): ")
   if character_choice.lower() == "o":
     is_circle = True
-  if character_choice.lower() == "x":
+  elif character_choice.lower() == "x":
     is_circle = False
-  # else:
-  #   choose_character()
-  return is_circle
+  else:
+    print("Wprowadziłeś coś innego niż 'X' lub 'O', spróbuj jeszcze raz")
+    choose_character()
+  return is_circle 
+
+def whos_first():
+  who = input("Kto zaczyna komputer czy Ty (K/T)? ")
+  if who == "k":
+    AI_game_k()
+  elif who == "t":
+    unbeatable_AI_game()
+  else:
+    print("Wbrałeś coś innego niż 'K' lub 'T', spróbuj jeszcze raz")
+    whos_first() 
 
 def game():
   GAME_ON = True
@@ -175,10 +184,139 @@ def game():
       print("Remis!")
       menu_again()
 
+
+def AI_game_k():
+  GAME_ON = True
+  is_circle = True
+  board = get_empty_board()
+  while GAME_ON:
+    if is_circle == True:
+      character = "X"
+    else:
+      character = "O"
+    get_random_ai_coordinates(character, board)
+    cls()
+    display_board(board)
+    if check_win(board):
+      GAME_ON = False
+      print("Wygrał gracz grający znakiem:", character)
+      menu_again()
+    if is_board_full(board):
+      GAME_ON = False
+      print("Remis!")
+      menu_again()
+    character = "O"
+    get_human_coordinates(character, board)
+    display_board(board)
+    if check_win(board):
+      GAME_ON = False
+      print("Wygrał gracz grający znakiem:", character)
+      menu_again()
+    if is_board_full(board):
+      GAME_ON = False
+      print("Remis!")
+      menu_again()
+
+
+def AI_game():
+  GAME_ON = True
+  is_circle = True
+  board = get_empty_board()
+  while GAME_ON:
+    if is_circle == True:
+      character = "X"
+    else:
+      character = "O"
+    get_human_coordinates(character, board)
+    display_board(board)
+    if check_win(board):
+      GAME_ON = False
+      print("Wygrał gracz grający znakiem:", character)
+      menu_again()
+    if is_board_full(board):  #
+      GAME_ON = False
+      print("Remis!")
+      menu_again()
+    character = "O"
+    get_random_ai_coordinates(character, board)
+    cls()
+    display_board(board)
+
+def get_random_ai_coordinates(character, board):
+  random_ai_input_row = random.randint(0, 2)
+  random_ai_input_col = random.randint(0, 2)
+  random_ai_coordinates_tuple = (random_ai_input_row, random_ai_input_col)
+  if board[random_ai_coordinates_tuple[0]][
+      random_ai_coordinates_tuple[1]] == ".":
+    board[random_ai_coordinates_tuple[0]][
+      random_ai_coordinates_tuple[1]] = character
+  elif board[random_ai_coordinates_tuple[0]][
+      random_ai_coordinates_tuple[1]] == "X" or board[
+        random_ai_coordinates_tuple[0]][random_ai_coordinates_tuple[1]] == "O":
+    get_random_ai_coordinates(character, board)
+
+def unbeatable_AI_game():
+  GAME_ON = True
+  is_circle = True
+  board = get_empty_board()
+  while GAME_ON:
+    if is_circle == True:
+      character = "X"
+    else:
+      character = "O"
+    get_human_coordinates(character, board)
+    display_board(board)
+    if check_win(board):
+      GAME_ON = False
+      print("Wygrał gracz grający znakiem:", character)
+      menu_again()
+    if is_board_full(board):
+      GAME_ON = False
+      print("Remis!")
+      menu_again()
+    character = "O"
+    get_unbeatable_ai_coordinates(character, board)
+    cls()
+    display_board(board)
+    if check_win(board):
+      GAME_ON = False
+      print("Wygrał gracz grający znakiem:", character)
+      menu_again()
+    if is_board_full(board):
+      GAME_ON = False
+      print("Remis!")
+      menu_again()
+
+def get_unbeatable_ai_coordinates(character, board):
+  if board[0][0] == ".":
+    board[0][0] = character
+  if board[1][1] == "X" and board[0][1] == "X":
+    board[2][1] = character
+  if board[1][1] == "X" and board[1][0] == "X":
+    board[1][2] = character
+  if board[1][1] == "X" and board[1][2] == "X":
+    board[1][0] = character
+  if board[1][1] == "X" and board[2][1] == "X":
+    board[0][1] = character
+  if board[1][1] == "X" and board[0][2] == "X":
+    board[2][0] = character
+  if board[1][1] == "X" and board[2][0] == "X":
+    board[0][2] = character
+  elif board[1][1] == ".":
+    board[1][1] = character
+  if board[0][0] == "X" and board[0][1] == "X":
+    board[0][2] = character
+  if board[0][0] == "X" and board[0][2] == "X":
+    board[0][1] = character
+  if board[0][0] == "X" and board[1][0] == "X":
+    board[2][0] = character
+  if board[0][0] == "X" and board[2][0] == "X":
+    board[1][0] = character
+  if board[1][1] == "X" and board[0][2] == "X":
+    board[2][0] = character
+  if board[1][1] == "X" and board[2][0] == "X":
+    board[0][2] = character
+
 main_menu()
 
-# def AI_game()
-
-# poprawić quit ==> na razie tylko do wpisywania rzędu wyjście jest niezbyt eleganckie (repl process died unexpectedly)
 # uprościć check_win
-# zrobić algorytm do gry z komputerem
